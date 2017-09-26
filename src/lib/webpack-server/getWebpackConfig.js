0efca7776986c1c213ed0a46820c83684e48fa76
yucong02 <yucong02@baidu.com>
2017-09-26 19:44:39 +0800

    dev webpack-server

--- src/lib/webpack-server/getWebpackConfig.js
+++ src/lib/webpack-server/getWebpackConfig.js
@@ -6,17 +6,15 @@ import ExtractTextWebpackPlugin from 'extract-text-webpack-plugin'
 
 export default function getWebpackConfig (opt) {
 
-
-
     return {
         babel: {
             cacheDirectory: true,
-                presets: [
+            presets: [
                 require.resolve('babel-preset-es2015-ie'),
                 require.resolve('babel-preset-react'),
-                require.resolve('babel-preset-stage-0'),
+                require.resolve('babel-preset-stage-0')
             ],
-                plugins: [
+            plugins: [
                 require.resolve('babel-plugin-add-module-exports'),
                 require.resolve('babel-plugin-transform-decorators-legacy'),
             ],
@@ -24,7 +22,7 @@ export default function getWebpackConfig (opt) {
 
         output: {
             path: join(process.cwd(), 'dist'),
-            filename: '',
+            filename: '[name]',
         },
 
         devtool: 'source-map',
