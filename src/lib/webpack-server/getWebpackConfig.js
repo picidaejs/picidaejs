1c04734a83c267386ffeb3cc9617d5bf1bc4c81c
yucong02 <yucong02@baidu.com>
2017-10-19 15:30:45 +0800

    added postcss-focus

--- src/lib/webpack-server/getWebpackConfig.js
+++ src/lib/webpack-server/getWebpackConfig.js
@@ -31,6 +31,7 @@ export default function getWebpackCommonConfig(args = {}) {
             autoprefixer({
                 browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4'],
             }),
+            require('postcss-focus')
         ],
     };
 
