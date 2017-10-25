2233451229924f9950de18bddf5cd32d09d11919
yucong02 <yucong02@baidu.com>
2017-10-25 17:19:24 +0800

    routesMap support function

---
+++ docs/docs/doc/outer.md
@@ -0,0 +1,8 @@
+---
+title: OUTER
+datetime: 2020-10-10 10:00
+---
+
+
+OUTER OUTER OUTER  
+[HOME](../home.md)
--- docs/theme-configs/default.js
+++ docs/theme-configs/default.js
@@ -2,10 +2,17 @@ module.exports = {
     hello: 'world -> default',
     pageSize: 10,
 
-    routesMap: {
-        'CHANGELOG.md': 'changelog.md',
-        'home.md': 'INDEX.md',
-        // 'doc': ''
-        // 'api': 'post',
+    routesMap: function (pathname) {
+        console.log(pathname);
+
+        var map = {
+            'CHANGELOG.md': 'changelog',
+            'home.md': 'INDEX.md',
+            // 'doc/index.md': 'changelog',
+            // 'doc': ''
+            // 'api': 'post',
+        };
+
+        return map[pathname];
     }
 }
