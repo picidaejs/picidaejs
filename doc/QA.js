8b47d6c8596e8ed3acc5ce6c900bd2049c064c47
Moyu <492899414@qq.com>
2017-11-10 00:19:53 +0800

    picidae-commander-gh-pages: 2017-11-10 00:19:43

--- doc/QA.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([6],{460:function(n,r){n.exports={content:'<p><a href="/index">HOME</a></p>\n<p><a href="/index">HOME</a></p>\n<p><a href="/doc/QA">../doc/QA.md</a><br>\n<a href="/doc/transformer">../doc/transformer.md</a>     </p>\n<ul>\n<li>为什么需要传入 require</li>\n</ul>\n',extra:{}}}});
